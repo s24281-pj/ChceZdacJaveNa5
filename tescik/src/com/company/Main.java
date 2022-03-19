7f49c7deb2c3f1e001ad943317ca2ec4f3dee270
s24281-pj <92219323+s24281-pj@users.noreply.github.com>
2022-03-19 13:10:35 +0100

    pierwszy

---
+++ tescik/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        {
+            String[] tablica = {"1", "1", "2", "1"};
+            //System.out.println(tablica[0] + tablica[1] + tablica[2] + tablica[3]);
+            int i;
+            for(i=0;  i<5; i++)
+            {
+                int wynik = Integer.parseInt(tablica);
+                System.out.println(wynik);
+            }
+            //System.out.println(wynik);
+        }
+    }
+}
