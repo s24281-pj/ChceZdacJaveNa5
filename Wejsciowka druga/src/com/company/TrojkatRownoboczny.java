c30df3b72cd2e839854dc82ad2dd8c16805af4e3
s24281-pj <92219323+s24281-pj@users.noreply.github.com>
2022-04-02 14:18:13 +0200

    Add files via upload

---
+++ Wejsciowka druga/src/com/company/TrojkatRownoboczny.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class TrojkatRownoboczny extends TrojkatRownoramienny {
+    public TrojkatRownoboczny(double podstawa) {
+    }
+
+    public TrojkatRownoboczny(double podstawa, double podstawa1) {
+
+    }
+
+
+    public int wysokosc() {
+        return 2;
+    }
+}
