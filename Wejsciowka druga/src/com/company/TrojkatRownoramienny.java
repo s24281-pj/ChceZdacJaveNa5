8ce7e2be612be5aea23e161e51a584fdb8607384
s24281-pj <92219323+s24281-pj@users.noreply.github.com>
2022-04-02 13:44:31 +0200

    Add files via upload

---
+++ Wejsciowka druga/src/com/company/TrojkatRownoramienny.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class TrojkatRownoramienny {
+    public TrojkatRownoramienny(double podstawa, double ramie) {
+    }
+
+    public TrojkatRownoramienny() {
+
+    }
+
+
+    public double wysokosc(double wyskosc) {
+        return 2.5;
+    }
+    public int wysokosc(int wyskosc) {
+        return 3;
+    }
+}
