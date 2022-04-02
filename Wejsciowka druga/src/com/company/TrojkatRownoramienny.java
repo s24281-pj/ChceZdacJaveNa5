c312487c1f5d6049ce0f5c24ccd28588c846e12c
s24281-pj <92219323+s24281-pj@users.noreply.github.com>
2022-04-02 13:29:32 +0200

    drugi commit

---
+++ Wejsciowka druga/src/com/company/TrojkatRownoramienny.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class TrojkatRownoramienny {
+    public TrojkatRownoramienny(double podstawa, double ramie) {
+    }
+
+    public TrojkatRownoramienny() {
+
+    }
+
+
+    public int wysokosc(double wyskosc) {
+        return 3;
+    }
+}
