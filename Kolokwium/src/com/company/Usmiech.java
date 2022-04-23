de87369966cebe1661d29f9aa99fd433b41102b8
s24281-pj <92219323+s24281-pj@users.noreply.github.com>
2022-04-23 13:50:35 +0200

    Add files via upload

---
+++ Kolokwium/src/com/company/Usmiech.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Usmiech extends Buzka implements Skalowalny {
+    public Usmiech(char c) {
+
+        super(')');
+    }
+
+    public Usmiech() {
+
+        super(')');
+    }
+
+    @Override
+    public void draw(int times) {
+
+    }
+}
